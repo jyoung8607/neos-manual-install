641d832cbc65ed1bb93f023ef136c7bee97eeed3
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2024-03-04 22:47:34 -0500

    Update c2setup.sh

--- c2setup.sh
+++ c2setup.sh
@@ -4,7 +4,8 @@ DEFAULT_GITHUB_OWNER="commaai"
 DEFAULT_GITHUB_BRANCH="release2"
 
 cd /data
-git clone https://github.com/"${1:-$DEFAULT_GITHUB_OWNER"}"/openpilot.git -b "${1:-$DEFAULT_GITHUB_BRANCH"}" --recurse-submodules --depth 1
+rm -rf openpilot
+git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/openpilot.git -b ${1:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1
 
 cd /data/data/com.termux/files
 echo $"#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n" > continue.sh
