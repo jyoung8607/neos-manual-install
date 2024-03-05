bee3291c8ef7ee31b2b3a627f7bd4a65b2cd04ec
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2024-03-04 23:19:25 -0500

    Update c2setup.sh

--- c2setup.sh
+++ c2setup.sh
@@ -10,7 +10,7 @@ rm -rf openpilot
 time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/openpilot.git -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1
 
 cd /data/data/com.termux/files
-echo $"#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n" > continue.sh
+echo $'#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
 chmod +x continue.sh
 echo "Install complete, rebooting..."
-# reboot
+reboot
